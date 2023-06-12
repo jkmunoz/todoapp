bbc487aea96d0bf3dc28bb0f8dfbf80603aff5e7
jkmunoz <juliannakmunoz@gmail.com>
2023-06-12 15:46:30 -0500

    differintiates b/w lists of todos and a todo list.

--- app.py
+++ app.py
@@ -80,9 +80,15 @@ def set_completed_todo(todo_id):
         db.session.close()
     return redirect(url_for('index'))
 
+@app.route('/lists/<list_id>')
+def get_list_todos(list_id):
+        return render_template('index.html', 
+        todos=Todo.query.filter_by(list_id=list_id).order_by('id')
+        .all())
+
 @app.route('/')
 def index():
-        return render_template('index.html', todos=Todo.query.order_by('id').all())
+    return redirect(url_for('get_list_todos',list_id=1))
 
 if __name__ == '__main__':
    app.run(host="0.0.0.0", port=5000)
