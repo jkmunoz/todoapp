a304dd71c703f25001d8295866b7fdd353cd46f3
jkmunoz <juliannakmunoz@gmail.com>
2023-06-13 15:10:09 -0500

    attempt to allow toggling and deleting tasks.

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import sys
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://Julianna@localhost:5432/todoapp'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# commenting out to try and fix toggling issue
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
     
@@ -13,11 +14,14 @@ class Todo(db.Model):
     __tablename__ = 'Todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
-    completed = db.Column(db.Boolean, nullable=True, default=False)
-    list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=True)
+    # removed nullable=True from b/w db.Boolean and default=False to fix toggle.
+    completed = db.Column(db.Boolean, default=False)
+    # changed nullable=True to False to attempt to fix toggle issue.
+    list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
 
     def __repr__(self):
-        return f'<Todo {self.id} {self.description}, list {self.list_id}>'
+        # removed ', list {self.list_id}' from between '.description}' and >'
+        return f'<Todo {self.id} {self.description}>'
 
 class TodoList(db.Model):
     __tablename__ = 'todolists'
@@ -32,6 +36,33 @@ class TodoList(db.Model):
  #   db.drop_all()
  #   db.create_all()
 
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    error = False
+    body = {}
+    try:
+        description = request.get_json()['description']
+        list_id = request.get_json()['list_id']
+        todo = Todo(description=description, completed=False)
+        active_list = TodoList.query.get(list_id)
+        todo.list = active_list
+        db.session.add(todo)
+        db.session.commit()
+        # commented out bottom 2 according to solution (part 2 #14)
+        # body['id'] = todo.id
+        # body['completed'] = todo.completed
+        body['description'] = todo.description
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if not error:
+        return jsonify(body)
+    else:
+        abort (500)
+    
 @app.route('/todos/<todo_id>', methods=['DELETE'])
 def delete_todo(todo_id):
     try:
@@ -42,29 +73,6 @@ def delete_todo(todo_id):
     finally:
         db.session.close()
     return jsonify({ 'success': True })
-
-@app.route('/todos/create', methods=['POST'])
-def create_todo():
-    error = False
-    body = {}
-    try:
-        description = request.get_json()['description']
-        todo = Todo(description=description, completed=False)
-        db.session.add(todo)
-        db.session.commit()
-        body['id'] = todo.id
-        body['completed'] = todo.completed
-        body['description'] = todo.description
-    except:
-        error = True
-        db.session.rollback()
-        print(sys.exc_info())
-    finally:
-        db.session.close()
-    if error:
-        abort (400)
-    else:
-        return jsonify(body)
 
 @app.route('/todos/<todo_id>/set-completed', methods=['POST'])
 def set_completed_todo(todo_id):
@@ -88,15 +96,10 @@ def set_completed_todo(todo_id):
 
 @app.route('/lists/<list_id>')
 def get_list_todos(list_id):
-    lists = []
-    todos = Todo.query.all()
-    active_list = list_id
-
-    return render_template(
-        'index.html',
-        todos=todos,
-        lists=lists,
-        active_list=active_list
+    return render_template('index.html',
+        lists=TodoList.query.all(),
+        active_list=TodoList.query.get(list_id),
+        todos=Todo.query.filter_by(list_id=list_id).order_by('id').all()
     )
 
 @app.route('/')
