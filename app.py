942702245291c8048c10084b47c9b96f89dac169
jkmunoz <juliannakmunoz@gmail.com>
2023-06-19 15:11:09 -0500

    connect to a local postgresql database in app.py.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_migrate import Migrate
 import sys
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://amy@localhost:5432/todoapp'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://Julianna@localhost:5432/fyyur'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
